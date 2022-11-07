2f465e704af6b0438868d69e07dd0a4badc0c131
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-07 09:07:41 +0530

    [Afzal] Added: Contact Model

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,7 +6,27 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Address Book");
+            Contact contact = new Contact()
+            {
+                FirstName = Console.ReadLine(),
+                LastName = Console.ReadLine(),
+                Address = Console.ReadLine(),
+                City = Console.ReadLine(),
+                State = Console.ReadLine(),
+                Zip = Convert.ToInt64(Console.ReadLine()),
+                PhoneNumber = Console.ReadLine(),
+                Email = Console.ReadLine(),
+            };
+            Console.WriteLine("Contact Details" + "\n" +
+                contact.FirstName + "\n" +
+                contact.LastName + "\n" +
+                contact.Address + "\n" +
+                contact.City + "\n" +
+                contact.State + "\n" +
+                contact.Zip + "\n" +
+                contact.PhoneNumber + "\n" +
+                contact.Email);
         }
     }
 }
