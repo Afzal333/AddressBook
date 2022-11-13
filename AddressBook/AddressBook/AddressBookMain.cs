cfba7e0028360b94f096efce3ffa9adf093adebd
Afzal Haroon Mohammed <afzalharoon.mohammed@bpktechmail.com>
2022-11-13 17:19:23 +0530

    [Afzal] Refactor: Switch options in Program File

--- AddressBook/AddressBook/AddressBookMain.cs
+++ AddressBook/AddressBook/AddressBookMain.cs
@@ -6,30 +6,59 @@ namespace AddressBook
 {
     public class AddressBookMain
     {
+        Contact contact = new Contact();
         public void CreateContact()
         {
 
-            Console.WriteLine("Address Book");
-            Contact contact = new Contact()
+            Console.WriteLine("Address Book");            
+            Console.WriteLine("Enter FirstName :");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter LastName :");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address :");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City :");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter State :");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter ZIP :");
+            contact.Zip = Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter PhoneNumber :");
+            contact.PhoneNumber = Console.ReadLine();
+            Console.WriteLine("Enter Email :");
+            contact.Email = Console.ReadLine();
+            Display();    
+        }
+
+        public void EditContact(string name)
+        {
+            if(contact.FirstName.Equals(name) || contact.LastName.Equals(name))
             {
-                FirstName = Console.ReadLine(),
-                LastName = Console.ReadLine(),
-                Address = Console.ReadLine(),
-                City = Console.ReadLine(),
-                State = Console.ReadLine(),
-                Zip = Convert.ToInt64(Console.ReadLine()),
-                PhoneNumber = Console.ReadLine(),
-                Email = Console.ReadLine(),
-            };
+                Console.WriteLine("1. Address \n 2.City \n 3. State \n 4.Zip \n 5.Phone number");
+                int options = Convert.ToInt32(Console.ReadLine());
+                switch (options)
+                {
+                    case 1:contact.Address = Console.ReadLine(); Display(); break;
+                    case 2:contact.City = Console.ReadLine(); Display(); break;
+                    case 3:contact.State = Console.ReadLine(); Display(); break;
+                    case 4:contact.Zip =Convert.ToInt32(Console.ReadLine()); Display(); break;
+                    case 5:contact.PhoneNumber = Console.ReadLine(); Display(); break;
+
+                }
+            }
+        }
+
+        public void Display()
+        {
             Console.WriteLine("Contact Details" + "\n" +
-                contact.FirstName + "\n" +
-                contact.LastName + "\n" +
-                contact.Address + "\n" +
-                contact.City + "\n" +
-                contact.State + "\n" +
-                contact.Zip + "\n" +
-                contact.PhoneNumber + "\n" +
-                contact.Email);
+               contact.FirstName + "\n" +
+               contact.LastName + "\n" +
+               contact.Address + "\n" +
+               contact.City + "\n" +
+               contact.State + "\n" +
+               contact.Zip + "\n" +
+               contact.PhoneNumber + "\n" +
+               contact.Email);
 
         }
     }
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,8 +6,31 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
-            AddressBookMain addressBookMain = new AddressBookMain();
-            addressBookMain.CreateContact();
+            bool flag = true;
+            while (flag = true)
+            {
+                Console.WriteLine("AddressBook");
+                Console.WriteLine("Enter the Option");
+                Console.WriteLine("1. Create Contact "+"\n"+
+                                  "2. Edit Contact"+"\n" );
+                int options = Convert.ToInt32(Console.ReadLine());
+                AddressBookMain addressBookMain = new AddressBookMain();
+                switch (options)
+                {
+                    case 1:
+                        addressBookMain.CreateContact(); 
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the Contct name");
+                        string name=Console.ReadLine();
+                        addressBookMain.EditContact(name);
+                        break;
+
+                }
+               
+
+            }
+           
            
         }
     }
